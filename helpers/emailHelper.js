4b13dbd7eff833a4035a04bb62cdf2e60994de6f
Alessio <asymons@uwaterloo.ca>
2018-11-29 07:07:47 -0500

    changed email host

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -8,7 +8,7 @@ module.exports = {
 
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
-            host: 'smtp.live.com',
+            host: process.env.EMAIL_HOST,
             port: 587,
             secure: false, // true for 465, false for other ports
             auth: {
